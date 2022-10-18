6e993b4e8716175b7fcd741b3bcaca3513dcad37
Yaroslav Vavilin <yarikvavilin@gmail.com>
2022-10-18 17:51:48 +0500

    fix with-router.tsx

--- src/app/providers/with-router.tsx
+++ src/app/providers/with-router.tsx
@@ -1,10 +1,10 @@
-import React, { Suspense } from "react";
-import { BrowserRouter } from "react-router-dom";
+import React, { Suspense } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-export const withRouter = (component: () => React.ReactNode) => () => (
-	<BrowserRouter>
-		<Suspense fallback="Loading...">
-			{component()}
-		</Suspense>
-	</BrowserRouter>
-);
+// eslint-disable-next-line react/display-name
+export const withRouter = (component: () => React.ReactNode) => () =>
+    (
+        <BrowserRouter>
+            <Suspense fallback="Loading...">{component()}</Suspense>
+        </BrowserRouter>
+    );
